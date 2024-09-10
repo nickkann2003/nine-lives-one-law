2ea1b523eef75c6db3fca7f12f8026eb68dd5366
Ryan Schaufelberger <rbs5340@g.rit.edu>
2024-09-10 13:48:55 -0400

    test

--- Nine Lives One Law/Assets/Scripts/EXP_Controller.cs
+++ Nine Lives One Law/Assets/Scripts/EXP_Controller.cs
@@ -18,6 +18,7 @@ public class EXP_Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //comment
         Vector2 velocity = Vector2.zero;
 
         if(Input.GetKey(KeyCode.W))
