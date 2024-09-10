16f428336106e4abefc57be69449084266d5adef
Nick Kannenberg (RIT Student) <nrk5987@rit.edu>
2024-09-10 13:24:32 -0400

    player character experimental

---
+++ Nine Lives One Law/Assets/Scripts/EXP_Controller.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EXP_Controller : MonoBehaviour
+{
+    // Variables
+    private Rigidbody2D rb;
+    private BoxCollider2D collider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        collider = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 velocity = Vector2.zero;
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            velocity.y += 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            velocity.y -= 1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            velocity.x -= 1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            velocity.x += 1;
+        }
+
+        rb.velocity = velocity;
+    }
+}
