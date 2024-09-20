e1d82eb734447579ddab21a3b29a5d7ee05ce5bf
rbs5340 <rbs5340@g.rit.edu>
2024-09-19 20:46:41 -0400

    New Input System
    Modified player class to handle the new input system, which allows for compatibility with keyboard and controller. Also added a fire function

--- Nine Lives One Law/Assets/Scripts/Player.cs
+++ Nine Lives One Law/Assets/Scripts/Player.cs
@@ -1,16 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
 
-    //Variables
+    //Attributes
     private Rigidbody2D rb;
     private BoxCollider2D collider;
     [SerializeField]
     private Animator playerAnimator;
+
+    //Controls
+    public PlayerControls playerControls;
+    private InputAction move;
+    //private InputAction look;
+    private InputAction fire;
+
+    //Variables
     public float moveSpeed;
+
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -29,27 +43,10 @@ public class Player : MonoBehaviour
     // Moves the player depending on move speed variable, tells animator if moving
     void MovePlayer()
     {
-        Vector2 velocity = Vector2.zero;
+        Vector2 moveDirection = move.ReadValue<Vector2>();
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            velocity.y += moveSpeed;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            velocity.y -= moveSpeed;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            velocity.x -= moveSpeed;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            velocity.x += moveSpeed;
-        }
-
-        rb.velocity = velocity;
-        if (velocity.magnitude > 0)
+        if (rb.velocity.magnitude > 0)
         {
             playerAnimator.SetBool("Moving", true);
         }
@@ -66,5 +63,34 @@ public class Player : MonoBehaviour
         float AngleRad = Mathf.Atan2(mousePos.y - transform.position.y, mousePos.x - transform.position.x);
         float AngleDeg = (180 / Mathf.PI) * AngleRad - 90;
         this.transform.rotation = Quaternion.Euler(0, 0, AngleDeg);
+    }
+
+    // Makes the player fire a bullet
+    void Fire(InputAction.CallbackContext context)
+    {
+        Debug.Log("Fire!");
+    }
+
+    // Enables player controls
+    private void OnEnable()
+    {
+        move = playerControls.Player.Move;
+        move.Enable();
+
+        fire = playerControls.Player.Fire;
+        fire.Enable();
+        fire.performed += Fire;
+
+        //look = playerControls.Player.Look;
+        //look.Enable();
+    }
+
+
+    // Disables player controls
+    private void OnDisable()
+    {
+        move.Disable();
+        fire.Disable();
+        //look.Disable();
     }
 }
