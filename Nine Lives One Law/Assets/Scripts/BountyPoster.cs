6399ed4a484f71b50a6b5bafb2a257da17ec7521
Nick Kannenberg (RIT Student) <nrk5987@rit.edu>
2024-11-12 13:46:58 -0500

    Menu adjustments, bounty added

--- Nine Lives One Law/Assets/Scripts/BountyPoster.cs
+++ Nine Lives One Law/Assets/Scripts/BountyPoster.cs
@@ -8,24 +8,49 @@ public class BountyPoster : MonoBehaviour
     [Header("References")]
     public GameObject bountyImage;
     public GameObject bountyShotImage;
-    public Animator bountyAnimator;
+    public GameObject bountyPosterHover;
 
     // Start is called before the first frame update
     void Start()
     {
        bountyImage.SetActive(true);
-       bountyShotImage.SetActive(true);
+       bountyShotImage.SetActive(false);
+       bountyPosterHover.SetActive(false);
     }
 
     public void ShootBounty()
     {
         bountyImage.SetActive(false);
         bountyShotImage.SetActive(true);
+        bountyPosterHover.SetActive(false);
+    }
+
+    public void HoverBounty()
+    {
+        bountyPosterHover.SetActive(true);
+        bountyImage.SetActive(false);
+    }
+
+    public void UnHoverBounty()
+    {
+        bountyImage.SetActive(true);
+        bountyPosterHover.SetActive(false);
     }
 
     public void ResetBounty()
     {
         bountyImage.SetActive(true);
-        bountyShotImage.SetActive(true);
+        bountyShotImage.SetActive(false);
+        bountyPosterHover.SetActive(false);
+    }
+
+    private void OnMouseEnter()
+    {
+        HoverBounty();
+    }
+
+    private void OnMouseExit()
+    {
+        UnHoverBounty();
     }
 }
